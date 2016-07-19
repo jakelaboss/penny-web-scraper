18c042273e029b1258056bdd7d665679538c72ab
Jake LaBossier <jakelaboss@gmail.com>
2016-07-19 13:40:45 -0400

    Corrrect Query and insert statments

--- connections.py
+++ connections.py
@@ -2,6 +2,7 @@ import time
 import psycopg2
 import sys
 import multiprocessing as mp
+import item_scraper
 from bs4 import BeautifulSoup, SoupStrainer
 from selenium import webdriver
 
@@ -9,15 +10,20 @@ conn = psycopg2.connect("dbname=items user=jakelaboss host='ec2-54-234-158-224.c
 cursor = conn.cursor()
 items = item_scraper.load(open(item_scraper.py,"rb"))
 
+
 for item in items:
-    city = item[0]
+    auction_price = item[0]
     price = item[1]
     info = item[2]
 
-    query =  "INSERT INTO items (info, city, price) VALUES (%s, %s, %s);"
-    data = (info, city, price)
+    query = "INSERT INTO items(auction_id, item_id, item_name, auction_price, value_price, win_time, url, winner, user_id) VALUES (%s, %s, %s, %s, %s, %s, %s);"
+
+    data = item_scraper(auction_price, item_id,item_name, auction_price, value_price, win_time, url, winner, user_id)
 
     cursor.execute(query, data)
+
+    conn.commit()
+    conn.close();
 
 def store(data):
 	conn = psycopg2.connect()
