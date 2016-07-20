e474d27fab30946bc8a714727de4288b7ad84568
Jake LaBossier <jakelaboss@gmail.com>
2016-07-20 12:11:42 -0400

    links update

--- links.py
+++ links.py
@@ -4,6 +4,7 @@ import sys
 import multiprocessing as mp
 import item_scraper
 import connections
+
 from bs4 import BeautifulSoup, SoupStrainer
 from selenium import webdriver
 
@@ -11,21 +12,20 @@ domain = 'http://www.quibids.com/en'
 links=[]
 used_links=[]
 data=[]
+password=[]
+insert = "INSERT INTO urls(domain, url, item_id, item_name) VALUES(%s, %s, %s, %s)"
 
-def dbquib():
+def getlink():
     try:
-        conn = psycopg2.connect("dbname=items user=jakelaboss host='ec2-54-234-158-224.compute-1.amazonaws.com' password='~r@mnUHPWv)00Cbju:?e<WM5q~2EBaeP'")
+        conn = psycopg2.connect()
     except:
         print "I am unable to connect to the database."
 
-cursor = conn.cursor()
+        cursor = conn.cursor()
 
-items = item_scraper.load(open(item_scraper.py,"rb"))
 
 cursor.execute("SELECT link FROM urls WHERE used_url = f")
-
-while link is not none:
-    link = cursor.fetchone()
+link = cursor.fetchone()
 
 conn.commit()
 conn.close();
